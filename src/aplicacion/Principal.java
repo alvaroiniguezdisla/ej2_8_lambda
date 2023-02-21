8247bce07e6c4caa0804762b23d7197908e707f3
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 16:30:45 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("La suma de los elementos pares de la lista es: " + dominio.ej2_8_lambda.sumalistapares(lista));
+    }
 }
--- src/dominio/ej2_8_lambda.java
+++ src/dominio/ej2_8_lambda.java
@@ -1,2 +1,7 @@
-package dominio;public class ej2_8_lambda {
+package dominio;
+import java.util.stream.Stream;
+public class ej2_8_lambda {
+    public static int sumalistapares (int[] lista){
+        return  Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).reduce(0,(a,b)->a+b);
+    }
 }
